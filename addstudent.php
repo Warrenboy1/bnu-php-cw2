5b6fce76568d1b5267bfbee806d5aa9fd7c3588e
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2022-05-10 02:46:43 +0100

    Task 4

--- addstudent.php
+++ addstudent.php
@@ -7,14 +7,15 @@
 
 
     // check logged in
-    if (isset($_SESSION['id'])) {
+if (isset($_SESSION['id'])) {
 
-    echo template("templates/partials/header.php");
-    echo template("templates/partials/nav.php");
+echo template("templates/partials/header.php");
+echo template("templates/partials/nav.php");
 
    
-        // if the form has been submitted
-        if (isset($_POST['submit'])) {
+    // if the form has been submitted
+    if (isset($_POST['submit'])) {
+
         $firstname = $_POST['txtfirstname'];
         $lastname = $_POST['txtlastname'];
         $house = $_POST['txthouse'];
@@ -24,27 +25,24 @@
         $postcode = $_POST['txtpostcode'];
         $studentid = $_POST['txtstudentid'];
 
-        // build an sql statment to update the student details
-        $sql = "INSERT INTO student (firstname, lastname, house, town,
-        county, country, postcode, studentid) VALUES ($firstname,
-        $lastname, $house, $town, $county, $country, $postcode, $studentid)";
+        // prepare an sql statment to insert the student details
+        $stmt = $conn->prepare("INSERT INTO student (firstname, lastname, house, town,
+        county, country, postcode, studentid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
       
-        if(mysqli_query($conn,$sql)) {
-          echo "New record successfully created!";
-        }else{
-          echo "Error: " . $sql . ":-" . mysqli_error($conn);
-        }
-      
+        // This function binds the parameters to the SQL query and tells the database what the parameters are.
+        // The s character tells mysql that the parameter is a string. i for integar.
+        $stmt->bind_param("sssssssi", $firstname, $lastname, $house, $town, $county, $country, $postcode, $studentid);
 
-      $data['content'] = "<p>Your details have been updated</p>";
+        // Execute the statement
+        $stmt->execute();
+        $data['content'] = "<p>Your details have been updated</p>";
+    }
+        // if the profile picture form has been submitted
+    else if (isset($_POST['change'])) {
 
+                                                    echo;
     }
     else {
-      // Build a SQL statment to return the student record with the id that
-      // matches that of the session variable.
-      //$sql = "select * from student where studentid='". $_SESSION['id'] . "';";
-      //$result = mysqli_query($conn,$sql);
-      //$row = mysqli_fetch_array($result);
 
       // using <<<EOD notation to allow building of a multi-line string
       // see http://stackoverflow.com/questions/6924193/what-is-the-use-of-eod-in-php for info
@@ -72,14 +70,21 @@
         <input type="submit" value="Save" name="submit"/>
         </form>
 
+        <h2>Add Profile Picture to Student?</h2>
+        <form action="" method="post">
+        Profile Picture :
+        <input name="profilePicture" type="file" /><br/>
+        Input student id to match to database :
+        <input name="txtstudentid" type="number" /><br/>
+        <input type="submit" value="Change!" name="change"/>
+        </form>
+
     EOD;
 
    }
 
-   // render the template
-   echo template("templates/default.php", $data);
-
-
+    // render the template
+    echo template("templates/default.php", $data);
     echo template("templates/partials/footer.php");
 }
 ?>
